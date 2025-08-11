70c5209bc9c9e728fb81bc057bc47b97c86e5a52
behnam farhadi <farhadi.behnam@gmail.com>
2025-08-11 15:24:51 -0230

    Updating App

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import "./styles/main.css"; //
 import "./styles/Login.css"; // استایل هدر
 import Login from "./pages/login"; //
 import ClientLogin from "./pages/ClientLogin";
+import Search from "./components/Search";
+import Venues from "./pages/Venues";
 
 import Home from "./pages/Home";
 import Header from "./components/Header"; // کامپوننت هدر برای ناوبری
@@ -20,6 +22,7 @@ const App = () => {
         <Route path="/" element={<Home />} /> {/*مسیر صفحه اصلی */}
         <Route path="/login" element={<Login />} />
         <Route path="/client-login" element={<ClientLogin />} />
+        <Route path="/venues" element={<Venues />} />
         {/*مسیر لاگین */}
       </Routes>
     </Router>
