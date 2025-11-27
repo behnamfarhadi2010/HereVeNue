b27ff9c430a53c41d15520f26daef6a06123e69d
behnam farhadi <farhadi.behnam@gmail.com>
2025-11-27 18:15:36 -0330

    Using Hashrouter insread of Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import VenueOwnerDashboard from "./components/VenueOwnerDashboard.jsx";
 import Footer from "./components/Footer.jsx";
 
